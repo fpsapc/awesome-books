47a7c2dd78e55cb58a4dd36b0fafceda35424ff5
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-03-21 21:00:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,74 @@
-// make an array for books
-let books = [];
+// Define class AwesomeBooks
+class AwesomeBooks {
+// call constructor
+  constructor() {
+    this.books = [];
+    this.titleInput = document.getElementById('title');
+    this.authorInput = document.getElementById('author');
+    this.addBtn = document.getElementById('addBtn');
+    this.bookList = document.getElementById('bookList');
 
-// make neccessary elments from index.html
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const addBtn = document.getElementById('addBtn');
-const bookList = document.getElementById('bookList');
+    this.displayBooks();
 
-// function display books
-function displayBooks() {
-  bookList.innerHTML = '';
-  books.forEach((book, index) => {
-    const li = document.createElement('li');
-    li.style.listStyle = 'none';
-    li.innerHTML = `${book.title} <br> ${book.author} <br>`;
-    const removeBtn = document.createElement('button');
-    removeBtn.textContent = 'Remove';
-    removeBtn.classList.add('removeBtn');
-    removeBtn.setAttribute('data-book-index', index);
-    li.appendChild(removeBtn);
-    bookList.appendChild(li);
-    const hr = document.createElement('hr');
-    bookList.appendChild(hr);
-  });
+    this.addBtn.addEventListener('click', () => {
+      this.addBook();
+    });
+
+    this.bookList.addEventListener('click', (event) => {
+      if (event.target.classList.contains('removeBtn')) {
+        const bIndex = event.target.dataset.bookIndex;
+        this.removeBook(bIndex);
+      }
+    });
+  }
+
+  // define displayBooks method for class
+  displayBooks() {
+    this.bookList.innerHTML = '';
+    this.books.forEach((book, index) => {
+      const li = document.createElement('li');
+      li.style.listStyle = 'none';
+      li.innerHTML = `"${book.title}" by ${book.author}`;
+      const removeBtn = document.createElement('button');
+      removeBtn.textContent = 'Remove';
+      removeBtn.classList.add('removeBtn');
+      removeBtn.setAttribute('data-book-index', index);
+      li.appendChild(removeBtn);
+      this.bookList.appendChild(li);
+      const hr = document.createElement('hr');
+      this.bookList.appendChild(hr);
+    });
+  }
+
+  // define addBook method for class
+  addBook() {
+    if (this.titleInput.value !== '' && this.authorInput.value !== '') {
+      const title = this.titleInput.value;
+      const author = this.authorInput.value;
+
+      this.books.push({ title, author });
+      localStorage.setItem('books', JSON.stringify(this.books));
+
+      this.titleInput.value = '';
+      this.authorInput.value = '';
+
+      this.displayBooks();
+    }
+  }
+
+  // define removeBook method for class
+  removeBook(bIndex) {
+    this.books.splice(bIndex, 1);
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.displayBooks();
+  }
 }
+
+// Instantiate the BookCollection class
+const awesomeBooks = new AwesomeBooks();
 
 // check local storage
 if (localStorage.getItem('books')) {
-  books = JSON.parse(localStorage.getItem('books'));
-  displayBooks();
+  awesomeBooks.books = JSON.parse(localStorage.getItem('books'));
+  awesomeBooks.displayBooks();
 }
-
-// function add button
-addBtn.addEventListener('click', () => {
-  if (titleInput.value !== '' && authorInput.value !== '') {
-    const title = titleInput.value;
-    const author = authorInput.value;
-
-    books.push({ title, author });
-    localStorage.setItem('books', JSON.stringify(books));
-
-    titleInput.value = '';
-    authorInput.value = '';
-
-    displayBooks();
-  }
-});
-
-// remove book from the collection
-bookList.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeBtn')) {
-    const bIndex = event.target.dataset.bookIndex;
-
-    books.splice(bIndex, 1);
-    localStorage.setItem('books', JSON.stringify(books));
-    displayBooks();
-  }
-});
